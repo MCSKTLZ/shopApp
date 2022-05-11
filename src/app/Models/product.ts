b0386a42b984c5b32f3cddec518df57a61da7bd6
MCSKTLZ <twomorrow01@gmail.com>
2022-05-11 15:07:39 +0200

    add slides + cards view

---
+++ src/app/Models/product.ts
@@ -0,0 +1,7 @@
+export class Product {
+    title: string;
+    category: string;
+    picture: string;
+    price: number;
+    currency: string;
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import SwiperCore, { Autoplay, Keyboard, Pagination, Scrollbar, Zoom } from 'swiper';
+import { Product } from "../Models/product"
+
+SwiperCore.use([Autoplay, Keyboard, Pagination, Scrollbar, Zoom]);
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +11,21 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  mySlides: any [] = [];
+  myProducts: Product[] = [];
+  constructor() {
+    this.mySlides.push("../../assets/images/slide1.jpg")
+    this.mySlides.push("../../assets/images/slide2.jpg")
+    this.mySlides.push("../../assets/images/slide3.jpg")
+
+    this.myProducts.push( {title: "Robe", category:"Summer", price: 45, currency:"€", picture:"../../assets/images/slide1.jpg"})
+
+    this.myProducts.push( {title: "Pantalon", category:"Summer", price: 75, currency:"€", picture:"../../assets/images/slide2.jpg"})
+
+    this.myProducts.push( {title: "Gant", category:"Winter", price: 25, currency:"€", picture:"../../assets/images/slide3.jpg"})
+
+    this.myProducts.push( {title: "Polo", category:"Summer", price: 80, currency:"€", picture:"../../assets/images/slide1.jpg"})
+
+  }
 
 }
